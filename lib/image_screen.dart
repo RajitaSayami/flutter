7224526e872a71bcc893240be7c9f124608e499e
RajitaSayami <rajitamanandhar123456789@gmail.com>
2025-03-18 07:44:41 +0545

    image add in appbar

--- lib/image_screen.dart
+++ lib/image_screen.dart
@@ -6,13 +6,25 @@ class ImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Image Screen"),),
+      appBar: AppBar(title: Text("Image Screen"),
+      leading: ClipOval(child: Image.asset("images/bernhardt.jpg",
+       height: 100, 
+       width: 100, 
+       fit: BoxFit.cover,)),
+      ),
       body: Column(
         children: [
           Image.network("https://picsum.photos/200/300",
           height: 500,
           width: 400,
-          fit: BoxFit.none,),
+          fit: BoxFit.cover,),
+          ClipOval(
+             child: Image.asset("images/shoe.jpg",
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,),
+          ),
+          
           
         ],
       ),
